8df7882062b0e765c30c7fc35d8a40958e9c0e3b
brkcrdk <brkcrdk@gmail.com>
2021-02-28 17:59:39 +0300

    Style updates.

---
+++ theme.js
@@ -0,0 +1,14 @@
+export const color = {};
+
+const size = {
+  desktop: 1280,
+  laptop: 1024,
+  tablet: 768,
+  phone: 601,
+  mini: 414,
+};
+
+export const device = Object.keys(size).reduce((acc, cur) => {
+  acc[cur] = `(max-width: ${size[cur] - 1}px)`;
+  return acc;
+}, {});
