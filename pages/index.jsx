31ddac83f8bbb9e408f3434232cd617d0b05abab
brkcrdk <brkcrdk@gmail.com>
2021-03-07 00:05:06 +0300

    Homepage updated.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,13 @@
 import styled from 'styled-components';
-import { color } from 'theme';
+
+import { Hero } from 'page-components/Home';
 
 export default function Home() {
   return (
     <Container>
-      <TestH1>Main Page</TestH1>
-      <H2>Main Page</H2>
+      <Hero />
     </Container>
   );
 }
 
 const Container = styled.div``;
-
-const TestH1 = styled.h1`
-  color: ${color.primary};
-  font-size: 48px;
-`;
-
-const H2 = styled.h2`
-  color: ${color.text500};
-  font-size: 36px;
-`;
