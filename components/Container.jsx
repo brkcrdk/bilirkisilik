6364a1dc9a21d7f7e83cbfd7cc9c87353958eca8
brkcrdk <brkcrdk@gmail.com>
2021-03-07 21:48:33 +0300

    Container classname updated for fix styled components extend feature.

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,18 +1,14 @@
 import styled from 'styled-components';
 
 const Container = ({ children, className }) => {
-  return (
-    <StyledContainer className={`container ${className}`}>
-      {children}
-    </StyledContainer>
-  );
+  return <StyledContainer className={className}>{children}</StyledContainer>;
 };
 
 export default Container;
 
 const StyledContainer = styled.section`
   display: grid;
-  max-width: 1400px;
+  max-width: 1200px;
   width: 100%;
   margin: 0 auto;
 `;
