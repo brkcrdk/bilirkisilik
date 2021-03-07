d5b4f64d3e7e796cd5ef5cdfcda2bbc20b0f0824
brkcrdk <brkcrdk@gmail.com>
2021-03-07 17:55:22 +0300

    Header updated.

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -1,13 +1,27 @@
+import styled from 'styled-components';
+import { Container } from 'components';
 import Logo from './Logo';
 import DesktopNavigation from './DesktopNavigation';
-import { Container } from 'components';
 
 const Header = () => (
-  <header>
-    <Container>
+  <StyledHeader>
+    <HeaderItems>
       <Logo />
       <DesktopNavigation />
-    </Container>
-  </header>
+    </HeaderItems>
+  </StyledHeader>
 );
 export default Header;
+
+const StyledHeader = styled.header`
+  position: absolute;
+  width: 100%;
+  z-index: 9999;
+  padding: 40px 0;
+`;
+
+const HeaderItems = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
---
+++ layout/Info.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Container } from 'components';
+const Info = () => <StyledInfo>Info is here</StyledInfo>;
+export default Info;
+
+const StyledInfo = styled(Container)`
+  display: flex;
+`;
