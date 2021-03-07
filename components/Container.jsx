ee853180658b2c0d28f310b52423d4c2aff93f53
brkcrdk <brkcrdk@gmail.com>
2021-03-07 17:55:09 +0300

    Container component made extendable

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 
-import { device } from 'theme';
-
-const Container = ({ children }) => {
-  return <StyledContainer className="container">{children}</StyledContainer>;
+const Container = ({ children, className }) => {
+  return (
+    <StyledContainer className={`container ${className}`}>
+      {children}
+    </StyledContainer>
+  );
 };
 
 export default Container;
 
 const StyledContainer = styled.section`
   display: grid;
-  max-width: 1200px;
+  max-width: 1400px;
   width: 100%;
   margin: 0 auto;
-  z-index: 9;
-  @media ${device.desktop} {
-    padding: 20px;
-  }
 `;
