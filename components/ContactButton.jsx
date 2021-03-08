89147bb2caf248f5d3c4dee629d7d134992d8b72
brkcrdk <brkcrdk@gmail.com>
2021-03-08 21:54:15 +0300

    Contact button updated.

--- components/ContactButton.jsx
+++ components/ContactButton.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import CustomLink from './CustomLink';
 
-const ContactButton = ({ icon, name, link }) => (
+const ContactButton = ({ icon, name, link, label }) => (
   <StyledContactButton
     href={link}
     alt={name}
@@ -8,13 +9,26 @@ const ContactButton = ({ icon, name, link }) => (
     target="_blank"
     rel="noopener noreferrer"
   >
-    <i className={icon} />
+    <ContactIcon className={icon} hasLabel={label} />
+    {label && <ContactLabel>{label}</ContactLabel>}
   </StyledContactButton>
 );
 
 export default ContactButton;
 
-const StyledContactButton = styled.a`
+const StyledContactButton = styled(CustomLink)`
   margin: 0 10px;
   font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const ContactIcon = styled.i`
+  margin-right: ${(p) => p.hasLabel && '8px'};
+`;
+const ContactLabel = styled.label`
+  font-size: 16px;
+  font-weight: 400;
 `;
