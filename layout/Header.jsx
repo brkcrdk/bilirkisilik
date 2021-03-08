eddd0ab07fefe95cdb92a79da324e9ae70d91d1d
brkcrdk <brkcrdk@gmail.com>
2021-03-08 22:20:29 +0300

    Header scroll animations added.

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -4,17 +4,16 @@ import { Container } from 'components';
 import Logo from './Logo';
 import DesktopNavigation from './DesktopNavigation';
 import Info from './Info';
+import { color } from 'theme';
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const scrollPosition = () => {
-      if (window.pageYOffset > 250) return setScrolled(true);
+      if (window.pageYOffset > 100) return setScrolled(true);
       return setScrolled(false);
     };
-    console.log(scrolled);
-
     window.addEventListener('scroll', scrollPosition);
     return () => {
       window.removeEventListener('scroll', scrollPosition);
@@ -24,9 +23,11 @@ const Header = () => {
   return (
     <StyledHeader>
       <Info />
-      <HeaderItems>
-        <Logo />
-        <DesktopNavigation />
+      <HeaderItems scrolled={scrolled}>
+        <HeaderContent scrolled={scrolled}>
+          <Logo lightLogo={!scrolled} width={scrolled ? 150 : 200} />
+          <DesktopNavigation />
+        </HeaderContent>
       </HeaderItems>
     </StyledHeader>
   );
@@ -40,10 +41,31 @@ const StyledHeader = styled.header`
   z-index: 9999;
 `;
 
-const HeaderItems = styled(Container)`
+const HeaderItems = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  background: ${(p) => (p.scrolled ? 'rgba(255,255,255,0.9)' : 'transparent')};
+  position: ${(p) => p.scrolled && 'fixed'};
+  top: 0;
+  animation-name: ${(p) => p.scrolled && 'fadeIn'};
+  animation-duration: 0.5s;
+  animation-iteration-count: 1;
+
+  @keyframes fadeIn {
+    from {
+      transform: translateY(-50px);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
+const HeaderContent = styled(Container)`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  color: #fff;
-  padding: 20px 0;
+  color: ${(p) => (p.scrolled ? color.text800 : 'white')};
 `;
