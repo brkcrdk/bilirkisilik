47ba567c8aec708e545ce8439f4a474cd270ab33
brkcrdk <brkcrdk@gmail.com>
2021-03-15 00:00:14 +0300

    Add MobileNavigation.

--- layout/MobileNavigation.jsx
+++ layout/MobileNavigation.jsx
@@ -1,2 +1,56 @@
-const MobileNavigation = () => <div>MobileNavigation is here</div>;
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import { device, color } from 'theme';
+import Hamburger from './Hamburger';
+import { useSidebar } from 'hooks';
+
+const MobileNavigation = () => {
+  const containerRef = useRef();
+  const { isSidebarOpen } = useSidebar(containerRef);
+
+  return (
+    <StyledNavigation
+      ref={containerRef}
+      animate={isSidebarOpen ? { right: 0 } : { right: '-100%' }}
+      transition={{ type: 'tween', duration: 0.6 }}
+    >
+      <Hamburger burgerColor={color.primary} />
+      <div />
+      <p>links</p>
+      <p>socials</p>
+    </StyledNavigation>
+  );
+};
 export default MobileNavigation;
+
+const StyledNavigation = styled(motion.nav)`
+  display: none;
+  position: fixed;
+  top: 0;
+  width: 50%;
+  max-width: 600px;
+  height: 100vh;
+  background: ${color.backgroundColor};
+  padding: 10px 0;
+
+  @media ${device.laptop} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+  @media ${device.tablet} {
+    display: flex;
+    width: 70%;
+  }
+  @media ${device.mini} {
+    width: 100%;
+  }
+
+  .hamburger-btn {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+  }
+`;
