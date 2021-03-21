63dd3bd1bc300871e08aaf478a44a93b703393af
brkcrdk <brkcrdk@gmail.com>
2021-03-21 23:26:55 +0300

    Add active state to accordion item

--- components/AccordionItem.jsx
+++ components/AccordionItem.jsx
@@ -2,32 +2,32 @@ import { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { color } from 'theme';
-const AccordionItem = ({ children, title, ...props }) => {
-  const [active, setActive] = useState(false);
+const AccordionItem = ({ children, title, active, ...props }) => {
+  const [open, setOpen] = useState(false);
   const [scrollHeight, setHeight] = useState(0);
   const contentRef = useRef();
 
   useEffect(() => {
-    if (active && contentRef?.current) {
+    if (open && contentRef?.current) {
       return setHeight(contentRef.current.scrollHeight);
     } else {
       return setHeight(0);
     }
-  }, [active]);
+  }, [open]);
 
   return (
     <StyledAccordionItem {...props}>
-      <AccordionHeader onClick={() => setActive(!active)}>
+      <AccordionHeader onClick={() => setOpen(!open)} active={active}>
         {title}
         <AnimatedIcon
           className="icon-chevron-down"
-          animate={active ? { rotate: -180 } : { rotate: 0 }}
+          animate={open ? { rotate: -180 } : { rotate: 0 }}
           transition={{ duration: 0.3, type: 'tween' }}
         />
       </AccordionHeader>
       <AccordionContent
         ref={contentRef}
-        animate={active ? { height: scrollHeight } : { height: 0 }}
+        animate={open ? { height: scrollHeight } : { height: 0 }}
         transition={{ duration: 0.3, type: 'tween' }}
       >
         {children}
@@ -49,6 +49,7 @@ const AccordionHeader = styled.span`
   color: ${color.primary};
   margin: 0;
   padding: 0;
+  font-weight: ${(p) => p.active && 800};
   i {
     font-size: 25px;
   }
