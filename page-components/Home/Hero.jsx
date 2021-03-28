bd844d900175305e6aeb7bdcaf0462ae9982c634
brkcrdk <brkcrdk@gmail.com>
2021-03-28 22:41:43 +0300

    Hero animate active slide.

--- page-components/Home/Hero.jsx
+++ page-components/Home/Hero.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { useSwiper } from 'hooks';
 import { Slider } from 'components';
 import CustomSlide from './CustomSlide';
-
 const Hero = () => {
   const settings = {
     loop: true,
   };
+  const [slider, setSlider] = useState();
+  const { active } = useSwiper(slider);
+  console.log(active);
   return (
     <StyledHero>
-      <Slider settings={settings}>
-        <CustomSlide />
-        <CustomSlide />
-        <CustomSlide />
+      <Slider settings={settings} setSlider={setSlider}>
+        {[0, 1, 2].map((slide) => (
+          <CustomSlide active={active === slide} key={slide} />
+        ))}
       </Slider>
     </StyledHero>
   );
