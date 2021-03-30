3c8ed2c7f7d8d26d15457b623e06d69f60bbd7bc
brkcrdk <brkcrdk@gmail.com>
2021-03-30 22:34:29 +0300

    Update hero header animation.

--- page-components/Home/Hero.jsx
+++ page-components/Home/Hero.jsx
@@ -1,72 +1,49 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { useSwiper } from 'hooks';
-import { Slider } from 'components';
-import CustomSlide from './CustomSlide';
+import { Slider, CustomLink, Container } from 'components';
 import { motion } from 'framer-motion';
+import { color, theme } from 'theme';
+import { animationVariants, itemAnimations } from './heroAnimations';
 const Hero = () => {
   const [animationName, setAnimationName] = useState('show');
   const settings = {
     loop: true,
   };
   const [slider, setSlider] = useState();
-  const { active } = useSwiper(slider);
-  console.log(active);
+  const { active, slideChange } = useSwiper(slider);
 
   const handleAnimation = () => {
     setAnimationName('hide');
-    return setTimeout(() => setAnimationName('show'), 1000);
+    return setTimeout(() => setAnimationName('show'), 1600);
   };
 
-  const animationVariants = {
-    hide: {
-      transition: {
-        staggerChildren: 0.4,
-        staggerDirection: -1,
-      },
-    },
-    show: {
-      transition: {
-        staggerChildren: 0.4,
-        staggerDirection: 1,
-      },
-    },
-  };
-
-  const itemAnimations = {
-    hide: {
-      opacity: 0,
-      x: -200,
-      transition: {
-        duration: 0.7,
-      },
-    },
-    show: {
-      opacity: 0.7,
-      x: 0,
-      transition: {
-        duration: 0.7,
-      },
-    },
-  };
+  slideChange(handleAnimation);
   return (
     <StyledHero>
       <Slider settings={settings} setSlider={setSlider}>
         {[0, 1, 2].map((slide) => (
-          <CustomSlide active={active === slide} key={slide} />
+          <HeroImage key={slide} src="/images/slider-placeholder/1.png" />
         ))}
       </Slider>
-      <HeroHeader
-        variants={animationVariants}
-        initial="hide"
-        animate={animationName}
-      >
-        <HeroTitle variants={itemAnimations}>Hero Title</HeroTitle>
-        <HeroDescription variants={itemAnimations}>
-          Hero Description
-        </HeroDescription>
-        <button onClick={handleAnimation}>Test</button>
-      </HeroHeader>
+      <Container>
+        <HeroHeader
+          variants={animationVariants}
+          initial="hide"
+          animate={animationName}
+        >
+          <HeroTitle variants={itemAnimations}>Hero Title</HeroTitle>
+          <HeroDescription variants={itemAnimations}>
+            Hero Description Hero Description Hero Description Hero
+            DescriptionHero DescriptionHero DescriptionHero DescriptionHero
+            Description
+          </HeroDescription>
+          <AnimatedButton variants={itemAnimations} onClick={handleAnimation}>
+            <HeroButton route="/">DEVAMINI OKU</HeroButton>
+          </AnimatedButton>
+        </HeroHeader>
+      </Container>
+      <HeroShadow />
     </StyledHero>
   );
 };
@@ -77,10 +54,54 @@ const StyledHero = styled.section`
 `;
 
 const HeroHeader = styled(motion.header)`
+  max-width: 50%;
   position: absolute;
-  bottom: 500px;
+  bottom: 25vh;
   z-index: 99;
-  left: 100px;
+  color: ${color.backgroundColor};
 `;
-const HeroTitle = styled(motion.h1)``;
-const HeroDescription = styled(motion.h4)``;
+
+const HeroImage = styled.img`
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
+`;
+
+const HeroTitle = styled(motion.h1)`
+  font-size: 64px;
+  font-family: Work Sans;
+  font-weight: 600;
+`;
+const HeroDescription = styled(motion.h4)`
+  font-size: 32px;
+  font-family: Work Sans;
+  margin: 24px 0;
+  font-weight: 400;
+  letter-spacing: -0.15px;
+`;
+
+const HeroShadow = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  background: rgba(0, 0, 0, 0.4);
+`;
+
+const AnimatedButton = styled(motion.div)``;
+
+const HeroButton = styled(CustomLink)`
+  font-size: 14px;
+  font-weight: 600;
+  transition: all 0.3s ease-in-out;
+  border-radius: ${theme.borderRadius};
+  padding: 10px;
+  margin-left: -10px;
+  &:hover {
+    background: ${color.backgroundColor};
+    color: ${color.primary};
+  }
+`;
