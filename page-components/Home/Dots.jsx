a6fb0edb168c6db7a186be3d0dd003aad45bbe12
brkcrdk <brkcrdk@gmail.com>
2021-03-31 22:31:34 +0300

    Add custom dots.

--- page-components/Home/Dots.jsx
+++ page-components/Home/Dots.jsx
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
-const Dots = () => <div>Dots is here</div>;
+import { motion } from 'framer-motion';
+import { Container } from 'components';
+import { color } from 'theme';
+
+const Dots = ({ slides, active }) => (
+  <Container>
+    <StyledDots>
+      {slides.map((slide, index) => (
+        <DotContainer key={slide.title} activeSlide={active === index}>
+          <Dot activeSlide={active === index} />
+        </DotContainer>
+      ))}
+    </StyledDots>
+  </Container>
+);
 export default Dots;
+
+const StyledDots = styled.div`
+  position: absolute;
+  bottom: 16vh;
+  z-index: 99;
+  display: flex;
+`;
+
+const DotContainer = styled.div`
+  width: ${(p) => (p.activeSlide ? '200px' : '10px')};
+  height: 10px;
+  position: relative;
+  background: ${(p) => (p.activeSlide ? color.text600 : color.backgroundColor)};
+  margin: 0 20px;
+  border-radius: 50px;
+
+  &:first-of-type {
+    margin-left: 0;
+  }
+`;
+
+const Dot = styled(motion.span)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 20%;
+  height: 100%;
+  background: red;
+  border-radius: 50px;
+`;
