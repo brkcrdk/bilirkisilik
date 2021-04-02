4f976f0f6a4941523ebe70af5626a0e6332c3c3c
brkcrdk <brkcrdk@gmail.com>
2021-04-02 21:08:47 +0300

    Fix style.

--- components/Map.jsx
+++ components/Map.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Map = () => (
   <StyledMap
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.3255761153155!2d32.741343315783425!3d39.95647297942126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMznCsDU3JzIzLjMiTiAzMsKwNDQnMzYuNyJF!5e0!3m2!1str!2str!4v1616524096218!5m2!1str!2str"
-    style="border:0;"
+    style="border:0"
     allowFullScreen=""
     loading="lazy"
   />
