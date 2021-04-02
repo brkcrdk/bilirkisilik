746e6b5df317d277eac59239e2484ab3d872e8f2
brkcrdk <brkcrdk@gmail.com>
2021-04-03 01:18:19 +0300

    Add section title component.

---
+++ components/SectionTitle.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { theme, color } from 'theme';
+const SectionTitle = ({ title = 'Title', description, ...props }) => (
+  <StyledSectionTitle {...props}>
+    <Header>
+      <h1>{title}</h1>
+      <h3>{title}</h3>
+    </Header>
+    {description && <span>{description}</span>}
+  </StyledSectionTitle>
+);
+export default SectionTitle;
+
+const StyledSectionTitle = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  span {
+    margin-top: 24px;
+    text-align: center;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: ${theme.font64};
+    color: ${color.text200};
+  }
+  h3 {
+    font-size: ${theme.font32};
+    position: absolute;
+    bottom: 0;
+    color: ${color.primary};
+  }
+`;
