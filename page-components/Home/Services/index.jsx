a5cac7649bcf49dc7346f3f62c28554485716306
brkcrdk <brkcrdk@gmail.com>
2021-04-04 00:51:32 +0300

    Refactor services section.

--- page-components/Home/Services/index.jsx
+++ page-components/Home/Services/index.jsx
@@ -1,15 +1,14 @@
-import { Container, SectionTitle } from 'components';
+import { Container, SectionTitle, SectionContainer } from 'components';
 import styled from 'styled-components';
 import { device } from 'theme';
 import ServicesCard from './ServicesCard';
 
 const Services = () => (
-  <StyledServices>
-    <SectionTitle
-      title="Hizmetlerimiz"
-      description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the is simply dummy text of the printing."
-    />
+  <SectionContainer
+    title="Hizmetlerimiz"
+    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the is simply dummy text of the printing."
+  >
     <ServiceCards>
       <ServicesCard />
       <ServicesCard />
@@ -20,23 +19,15 @@ const Services = () => (
       <ServicesCard />
       <ServicesCard />
     </ServiceCards>
-  </StyledServices>
+  </SectionContainer>
 );
 export default Services;
-
-const StyledServices = styled(Container)`
-  margin-top: 100px;
-  @media ${device.tablet} {
-    margin-top: 70px;
-  }
-`;
 
 const ServiceCards = styled.section`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 24px;
   padding: 0 24px;
-  margin-top: 100px;
   @media ${device.laptop} {
     grid-template-columns: repeat(3, 1fr);
     gap: 24px 12px;
@@ -45,7 +36,6 @@ const ServiceCards = styled.section`
   @media ${device.tablet} {
     grid-template-columns: repeat(2, 1fr);
     gap: 12px;
-    margin-top: 70px;
   }
   @media ${device.phone} {
     grid-template-columns: 1fr;
