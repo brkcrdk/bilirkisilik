6bfb7a95e63501718fe97f9ede4e767b69b025f1
brkcrdk <brkcrdk@gmail.com>
2021-04-04 00:51:40 +0300

    Refactor work with us.

--- page-components/Home/WorkWithUs/index.jsx
+++ page-components/Home/WorkWithUs/index.jsx
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
-import { Container, SectionTitle } from 'components';
+import { SectionContainer } from 'components';
 import { device } from 'theme';
 import WorkWithUsCard from './WorkWithUsCard';
 
 const WorkWithUs = () => (
-  <StyledWorkWithUs>
-    <SectionTitle
-      title="Neden Biz?"
-      description="Neden bizimle çalışmalısınız"
-    />
+  <SectionContainer
+    title="Neden Biz?"
+    description="Neden bizimle çalışmalısınız"
+  >
     <CardContainer>
       <WorkWithUsCard />
       <WorkWithUsCard />
@@ -17,28 +16,17 @@ const WorkWithUs = () => (
       <WorkWithUsCard />
       <WorkWithUsCard />
     </CardContainer>
-  </StyledWorkWithUs>
+  </SectionContainer>
 );
 export default WorkWithUs;
-
-const StyledWorkWithUs = styled(Container)`
-  margin-top: 100px;
-  @media ${device.tablet} {
-    margin-top: 70px;
-  }
-`;
 
 const CardContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 48px 24px;
-  margin-top: 100px;
   @media ${device.laptop} {
     grid-template-columns: repeat(2, 1fr);
     gap: 24px;
-  }
-  @media ${device.tablet} {
-    margin-top: 70px;
   }
   @media ${device.phone} {
     padding: 0 12px;
