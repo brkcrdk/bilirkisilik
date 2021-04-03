c8e925b30a3af44aff29b25db557867d5296ae3e
brkcrdk <brkcrdk@gmail.com>
2021-04-04 00:51:18 +0300

    Faq added.

---
+++ page-components/Home/Faq/index.jsx
@@ -0,0 +1,10 @@
+import { SectionContainer } from 'components';
+const Faq = () => (
+  <SectionContainer
+    title="S.S.S"
+    description="Sıkça sorulan sorularınızın cevapları."
+  >
+    FAQ Here
+  </SectionContainer>
+);
+export default Faq;
