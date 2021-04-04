fccee229b4fafebe4f29e9c1e0367820e3eed108
brkcrdk <brkcrdk@gmail.com>
2021-04-04 14:32:37 +0300

    Add Faq section to homepage.

--- page-components/Home/Faq/index.jsx
+++ page-components/Home/Faq/index.jsx
@@ -1,10 +1,62 @@
-import { SectionContainer } from 'components';
+import styled from 'styled-components';
+import { SectionContainer, AccordionItem } from 'components';
+import { color, theme } from 'theme';
+
 const Faq = () => (
-  <SectionContainer
+  <CustomSection
     title="S.S.S"
     description="Sıkça sorulan sorularınızın cevapları."
   >
-    FAQ Here
-  </SectionContainer>
+    <CustomAccordion title="Test">
+      <Content>Content</Content>
+    </CustomAccordion>
+    <CustomAccordion title="Test">
+      <Content>Content</Content>
+    </CustomAccordion>
+    <CustomAccordion title="Test">
+      <Content>Content</Content>
+    </CustomAccordion>
+    <CustomAccordion title="Test">
+      <Content>Content</Content>
+    </CustomAccordion>
+    <CustomAccordion title="Test">
+      <Content>Content</Content>
+    </CustomAccordion>
+  </CustomSection>
 );
 export default Faq;
+
+const CustomSection = styled(SectionContainer)`
+  .section-title {
+    margin-bottom: 50px;
+  }
+`;
+
+const CustomAccordion = styled(AccordionItem)`
+  margin: 6px 0;
+  border: 1px solid ${color.primary100};
+  padding: 12px;
+  border-radius: ${theme.borderRadius};
+  &:first-of-type {
+    margin-top: 0;
+  }
+  .accordion-content {
+    padding: 0;
+    margin: 0;
+    background: transparent;
+  }
+  .accordion-header {
+    font-weight: 700;
+    i {
+      font-size: 20px;
+      margin-left: 8px;
+    }
+  }
+`;
+
+const Content = styled.div`
+  padding: 12px;
+  cursor: auto;
+  color: ${color.secondary};
+  background: ${color.primary50};
+`;
