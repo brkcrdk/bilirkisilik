6875a615083ed8ff695bfcb08c4eb0c338a1a015
brkcrdk <brkcrdk@gmail.com>
2021-04-04 22:18:07 +0300

    Test data fetching with graphql.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
+import { gql } from '@apollo/client';
+import client from 'client';
 
 import { Hero, Services, WorkWithUs, Faq } from 'page-components/Home';
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <Container>
       <Hero />
@@ -14,3 +17,27 @@ export default function Home() {
 }
 
 const Container = styled.div``;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Posts {
+        posts {
+          id
+          createdBy {
+            id
+            name
+          }
+          title
+          slug
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      posts: data.posts,
+    },
+  };
+}
