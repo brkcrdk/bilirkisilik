1903bb1ebfddf970a34c53e97a55388d62f4a8a3
brkcrdk <brkcrdk@gmail.com>
2021-04-11 21:54:58 +0300

    Update map component.

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,18 +1,14 @@
 /* eslint-disable max-len */
-import styled from 'styled-components';
+import GoogleMapReact from 'google-map-react';
 
-const Map = () => (
-  <StyledMap
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.3255761153155!2d32.741343315783425!3d39.95647297942126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMznCsDU3JzIzLjMiTiAzMsKwNDQnMzYuNyJF!5e0!3m2!1str!2str!4v1616524096218!5m2!1str!2str"
-    style="border:0"
-    allowFullScreen=""
-    loading="lazy"
-  />
-);
+const Map = () => {
+  return (
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: 'AIzaSyAvYFSKMTb5CTg_g1gYRdVY9YWiaolXx2c' }}
+      defaultCenter={{ lat: 59.95, lng: 39.33 }}
+      defaultZoom={11}
+    />
+  );
+};
 
 export default Map;
-
-const StyledMap = styled.iframe`
-  width: 100%;
-  min-height: 200px;
-`;
