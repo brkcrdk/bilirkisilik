bd9f759efb95e4c8ea10759f982416499d8a6cc5
brkcrdk <brkcrdk@gmail.com>
2021-04-11 16:26:02 +0300

    Fetch and handle navigation on server side.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,52 @@
-import styled from 'styled-components';
 import { gql } from '@apollo/client';
 import client from 'client';
+import Layout from 'layout';
 
 import { Hero, Services, WorkWithUs, Faq } from 'page-components/Home';
 
-export default function Home({ posts }) {
-  console.log(posts);
+export default function Home({ navigation }) {
   return (
-    <Container>
+    <Layout navigation={navigation}>
       <Hero />
       <WorkWithUs />
       <Services />
       <Faq />
-    </Container>
+    </Layout>
   );
 }
-
-const Container = styled.div``;
 
 export async function getStaticProps() {
   const { data } = await client.query({
     query: gql`
-      query Posts {
-        posts {
-          id
-          createdBy {
-            id
-            name
-          }
-          title
-          slug
+      query Navigation {
+        navigasyons {
+          kategori
+          linkLabel
+          linkRoute
+          hasSubLinks
+          isMainLink
         }
       }
     `,
   });
 
+  const mainLinks = data.navigasyons.filter((item) => item.isMainLink === true);
+
+  const navigation = mainLinks.map((item) => {
+    if (item.hasSubLinks) {
+      return {
+        ...item,
+        subLinks: data.navigasyons.filter(
+          (link) => link.kategori === item.kategori && !link.hasSubLinks
+        ),
+      };
+    }
+    return item;
+  });
+
   return {
     props: {
-      posts: data.posts,
+      navigation,
     },
   };
 }
