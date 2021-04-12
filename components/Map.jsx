4d8d49b1c9cc45aece67150d2e4bc6f77310d16f
brkcrdk <brkcrdk@gmail.com>
2021-04-12 22:38:47 +0300

    Add custom icon pin to map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,14 +1,25 @@
-/* eslint-disable max-len */
+import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
+import { theme } from 'theme';
 
-const Map = () => {
+const Map = ({ coordinations }) => {
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: 'AIzaSyAvYFSKMTb5CTg_g1gYRdVY9YWiaolXx2c' }}
-      defaultCenter={{ lat: 59.95, lng: 39.33 }}
-      defaultZoom={11}
-    />
+      defaultCenter={{ lat: coordinations.lat, lng: coordinations.lng }}
+      defaultZoom={14}
+    >
+      <CustomMarker
+        lat={coordinations.lat}
+        lng={coordinations.lng}
+        className="icon-map-pin"
+      />
+    </GoogleMapReact>
   );
 };
 
 export default Map;
+
+const CustomMarker = styled.i`
+  font-size: ${theme.font20};
+`;
