f470670150c72d06a7ac76a74c096790ea42d30a
brkcrdk <brkcrdk@gmail.com>
2021-04-13 23:21:52 +0300

    Get social data from store.

--- components/SocialButtons.jsx
+++ components/SocialButtons.jsx
@@ -1,19 +1,41 @@
+import { useContext } from 'react';
 import { ExternalLink } from 'components';
 import styled from 'styled-components';
+import { SettingsContext } from 'context';
 
-const SocialButtons = ({ socialData, ...props }) => (
-  <StyledSocialButtons className="social-btns" {...props}>
-    {socialData?.map((data) => (
-      <ExternalLink
-        key={data.name}
-        name={data.name}
-        link={data.link}
-        icon={data.icon}
-        label={data.label}
-      />
-    ))}
-  </StyledSocialButtons>
-);
+const SocialButtons = ({ ...props }) => {
+  const { settings } = useContext(SettingsContext);
+  const socialData = [
+    {
+      name: 'Twitter',
+      icon: 'icon-twitter',
+      link: settings?.twitter,
+    },
+    {
+      name: 'Facebook',
+      icon: 'icon-facebook',
+      link: settings?.facebook,
+    },
+    {
+      name: 'Instagram',
+      icon: 'icon-instagram',
+      link: settings?.instagram,
+    },
+  ];
+  return (
+    <StyledSocialButtons className="social-btns" {...props}>
+      {socialData?.map((data) => (
+        <ExternalLink
+          key={data.name}
+          name={data.name}
+          link={data.link}
+          icon={data.icon}
+          label={data.label}
+        />
+      ))}
+    </StyledSocialButtons>
+  );
+};
 export default SocialButtons;
 
 const StyledSocialButtons = styled.div`
