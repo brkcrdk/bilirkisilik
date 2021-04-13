9f1d5b5e98325b95cfc35d0672f723189e95e37d
brkcrdk <brkcrdk@gmail.com>
2021-04-13 23:21:45 +0300

    Get contact data from store.

--- components/ContactButtons.jsx
+++ components/ContactButtons.jsx
@@ -1,19 +1,37 @@
+import { useContext } from 'react';
 import styled from 'styled-components';
 import { ExternalLink } from 'components';
-
-const ContactButtons = ({ contactData, ...props }) => (
-  <StyledContactButtons className="contact-btns" {...props}>
-    {contactData?.map((data) => (
-      <ExternalLink
-        key={data.name}
-        name={data.name}
-        link={data.link}
-        icon={data.icon}
-        label={data.label}
-      />
-    ))}
-  </StyledContactButtons>
-);
+import { SettingsContext } from 'context';
+const ContactButtons = ({ ...props }) => {
+  const { settings } = useContext(SettingsContext);
+  const contactData = [
+    {
+      name: 'Mail',
+      icon: 'icon-mail',
+      link: `mailto:${settings?.email}`,
+      label: settings?.email,
+    },
+    {
+      name: 'Phone',
+      icon: 'icon-phone',
+      link: `tel:${settings?.phoneNumber}`,
+      label: settings?.phoneNumber,
+    },
+  ];
+  return (
+    <StyledContactButtons className="contact-btns" {...props}>
+      {contactData?.map((data) => (
+        <ExternalLink
+          key={data.name}
+          name={data.name}
+          link={data.link}
+          icon={data.icon}
+          label={data.label}
+        />
+      ))}
+    </StyledContactButtons>
+  );
+};
 export default ContactButtons;
 
 const StyledContactButtons = styled.div`
