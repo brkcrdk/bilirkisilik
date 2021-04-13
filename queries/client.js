1d2ea9dd6e8146ad6d5d5d264e32afe04a5def43
brkcrdk <brkcrdk@gmail.com>
2021-04-14 00:13:37 +0300

    Move api url to env file.

--- queries/client.js
+++ queries/client.js
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri:
-    'https://api-eu-central-1.graphcms.com/v2/cknbxmfzxgfbh01xm6dti8u4l/master',
+  uri: process.env.NEXT_PUBLIC_API_KEY,
   cache: new InMemoryCache(),
 });
 
