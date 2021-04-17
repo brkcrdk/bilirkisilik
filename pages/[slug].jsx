2dd7588419930100b7c6281e66ad6aaa718094e7
brkcrdk <brkcrdk@gmail.com>
2021-04-17 19:40:57 +0300

    Add inner pages.

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,5 +1,29 @@
-const DetailPage = () => {
-  return <div>This is detail page</div>;
-};
+import Layout from 'layout';
+import { handleNavigation, footerNavigation } from 'utils';
+import { pageSettings } from 'queries';
+
+function DetailPage({ settings }) {
+  return (
+    <Layout settings={settings}>
+      <div>This is detail page</div>;
+    </Layout>
+  );
+}
 
 export default DetailPage;
+
+export async function getServerSideProps() {
+  const settings = await pageSettings();
+  const footerNav = footerNavigation(settings.navigasyons);
+  const navigation = handleNavigation(settings.navigasyons);
+
+  return {
+    props: {
+      settings: {
+        navigation,
+        footerNav,
+        settings: settings.settings[0],
+      },
+    },
+  };
+}
