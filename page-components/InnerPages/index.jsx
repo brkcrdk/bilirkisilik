527c9003e8863357d7d49f5b69afbc277a765b40
brkcrdk <brkcrdk@gmail.com>
2021-04-17 23:54:01 +0300

    Update inner page content.

--- page-components/InnerPages/index.jsx
+++ page-components/InnerPages/index.jsx
@@ -1,4 +1,48 @@
-function InnerPages() {
-  return <div>InnerPages is here</div>;
+import styled from 'styled-components';
+import { color, theme } from 'theme';
+
+function InnerPages({ data }) {
+  const { headerImage, headerTitle, headerDescription } = data;
+  return (
+    <StyledInnerPages>
+      <Header>
+        <img src={headerImage.url} />
+        <HeaderContent>
+          <h1>{headerTitle}</h1>
+          <span>{headerDescription}</span>
+        </HeaderContent>
+      </Header>
+    </StyledInnerPages>
+  );
 }
 export default InnerPages;
+
+const StyledInnerPages = styled.div``;
+
+const Header = styled.header`
+  position: relative;
+  img {
+    height: 60vh;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const HeaderContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: ${color.backgroundColor};
+  text-align: center;
+  font-family: Work Sans;
+
+  h1 {
+    font-size: ${theme.font48};
+    font-weight: 400;
+  }
+  span {
+    font-size: ${theme.font16};
+    font-weight: 100;
+  }
+`;
