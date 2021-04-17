c7a215154d6ce3be0df3545a988ae5236f15109d
brkcrdk <brkcrdk@gmail.com>
2021-04-18 01:42:19 +0300

    Service-worker added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 import { Providers } from 'context';
 import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <Providers>
       <AnimatePresence exitBeforeEnter>
