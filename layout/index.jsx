7be9223fc48cd917f4445a9286c3ff6bad7f5c32
brkcrdk <brkcrdk@gmail.com>
2021-04-17 19:17:41 +0300

    Update layout meta tags.

--- layout/index.jsx
+++ layout/index.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children, settings }) => {
   const {
     navigation,
     footerNav,
-    settings: { siteName, siteDescription },
+    settings: { siteName, siteDescription, siteLogo, siteIcon },
   } = settings;
 
   const { setSettings } = useContext(SettingsContext);
@@ -22,26 +22,18 @@ const Layout = ({ children, settings }) => {
       <Head>
         <title>{siteName}</title>
         <meta name="description" content={siteDescription} />
+        <link rel="shortcut icon" href={siteIcon.url} />
 
         <meta property="og:title" content={siteName} />
         <meta property="og:description" content={siteDescription} />
         <meta property="og:type" content="website" />
-        {/* <meta
-          property="og:image"
-          content={process.env.NEXT_PUBLIC_BASE_URL + meta?.image}
-        /> */}
-        {/* <meta
-          property="og:url"
-          content={process.env.NEXT_PUBLIC_BASE_URL + asPath}
-        /> */}
-        <meta property="og:site_name" content="90Pixel" />
+        <meta property="og:image" content={siteLogo.url} />
+
+        <meta property="og:site_name" content={siteName} />
 
         <meta property="twitter:title" content={siteName} />
         <meta property="twitter:description" content={siteDescription} />
-        {/* <meta
-          property="twitter:image"
-          content={process.env.NEXT_PUBLIC_BASE_URL + meta?.image}
-        /> */}
+        <meta property="twitter:image" content={siteLogo.url} />
         <meta name="twitter:image:alt" content={siteName} />
         <meta name="twitter:site" content={siteName} />
         <meta name="twitter:card" content="summary_large_image" />
