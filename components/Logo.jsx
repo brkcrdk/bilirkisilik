5ddb58e45206e217f1c63429b3fc5b18f32db299
brkcrdk <brkcrdk@gmail.com>
2021-04-17 19:18:03 +0300

    Fetch logos from cms.

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { theme } from 'theme';
-const Logo = ({ lightLogo, width = 200 }) => (
-  <Link href="/" passHref>
-    <a title="Logo" alt="logo">
-      <StyledLogo
-        src={`/images/${lightLogo ? 'light-logo' : 'logo'}.png`}
-        width={width}
-      />
-    </a>
-  </Link>
-);
+import { SettingsContext } from 'context';
+
+function Logo({ lightLogo, width = 200 }) {
+  const {
+    settings: { siteLogo, siteLightLogo },
+  } = useContext(SettingsContext);
+  return (
+    <Link href="/" passHref>
+      <a title="Logo" alt="logo">
+        <StyledLogo
+          src={lightLogo ? siteLightLogo?.url : siteLogo?.url}
+          width={width}
+        />
+      </a>
+    </Link>
+  );
+}
 export default Logo;
 
 const StyledLogo = styled.img`
