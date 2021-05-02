94f5d239d9710006ec264066753f5140e578b9a7
brkcrdk <brkcrdk@gmail.com>
2021-05-02 23:13:49 +0300

    Test serving google fonts seperatly.

--- pages/_document.js
+++ pages/_document.js
@@ -31,12 +31,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="tr">
         <Head>
-          {/* <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap"
             rel="stylesheet"
-          /> */}
-          <link href="/fonts/Nunito/nunito.css" rel="stylesheet" />
-          <link href="/fonts/WorkSans/work-sans.css" rel="stylesheet" />
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* <link href="/fonts/Nunito/nunito.css" rel="stylesheet" />
+          <link href="/fonts/WorkSans/work-sans.css" rel="stylesheet" /> */}
           <link href="/styles/index.css" rel="stylesheet" />
           <link href="/manifest.json" rel="manifest" />
           <link rel="apple-touch-icon" href="/images/icon/192x192.png" />
