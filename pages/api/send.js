b25335842b9e8467ef88d350178c3a7122a85fc6
brkcrdk <brkcrdk@gmail.com>
2021-05-08 01:12:41 +0300

    Add mail handler.

---
+++ pages/api/send.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+export default async function send(req, res) {
+  const transporter = nodemailer.createTransport({
+    pool: true,
+    host: 'smtpout.secureserver.net',
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user: 'info@kriminalincelemeleri.com',
+      pass: 'yesim.yildiz12',
+    },
+  });
+  const mailSend = await transporter.sendMail({
+    from: 'brkcrdk@gmail.com',
+    to: 'info@kriminalincelemeleri.com',
+    subject: 'Test Email Subject',
+    html: '<h1>Example HTML Message Body</h1>',
+  });
+
+  if (mailSend.accepted.length)
+    return res.status(200).json({ message: 'Mesajınız iletildi' });
+  return res.status(200).json({ message: 'Bir hata oluştu' });
+}
