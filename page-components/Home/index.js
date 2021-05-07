13872630d29a05ef6b9884c0ed59165d509e3d5c
brkcrdk <brkcrdk@gmail.com>
2021-05-08 01:12:29 +0300

    Add component form to homage.

--- page-components/Home/index.js
+++ page-components/Home/index.js
@@ -2,3 +2,4 @@ export { default as Hero } from './Hero/index';
 export { default as Services } from './Services/index';
 export { default as WorkWithUs } from './WorkWithUs/index';
 export { default as Faq } from './Faq/index';
+export { default as Form } from './Form';
