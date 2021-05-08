de56f90982bd96fcb0a081a4f4331826e81c542b
brkcrdk <brkcrdk@gmail.com>
2021-05-08 19:27:04 +0300

    Update form components style

--- components/Textarea.jsx
+++ components/Textarea.jsx
@@ -1,4 +1,16 @@
-function Textarea() {
-  return <div>Textarea is here</div>;
+import styled from 'styled-components';
+
+function Textarea({ ...props }) {
+  return (
+    <StyledTextarea {...props}>
+      <CustomTextarea />
+    </StyledTextarea>
+  );
 }
 export default Textarea;
+
+const StyledTextarea = styled.div``;
+
+const CustomTextarea = styled.textarea`
+  width: 100%;
+`;
