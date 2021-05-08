5cdc800eb24d3acb7a0e393c1a9f6b484f2bb53c
brkcrdk <brkcrdk@gmail.com>
2021-05-08 23:29:43 +0300

    Update text areas style.

--- components/Textarea.jsx
+++ components/Textarea.jsx
@@ -1,18 +1,68 @@
 import styled from 'styled-components';
+import { color, theme } from 'theme';
 
-function Textarea({ ...props }) {
+function Textarea({
+  error,
+  title = 'Test',
+  placeholder = 'Mesajınızı giriniz',
+  value,
+  onChange,
+  ...props
+}) {
   return (
     <StyledTextarea {...props}>
-      <CustomTextarea />
+      <InputTitle>{title}</InputTitle>
+      <CustomTextarea
+        hasError={error}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <InputError>{error}</InputError>
     </StyledTextarea>
   );
 }
 export default Textarea;
 
-const StyledTextarea = styled.div``;
+const StyledTextarea = styled.div`
+  position: relative;
+`;
 
 const CustomTextarea = styled.textarea`
   width: 100%;
   min-height: 100px;
   resize: none;
+  padding: 10px 10px;
+  border-color: ${(p) => (p.hasError ? color.red : color.text300)};
+  border-radius: ${theme.borderRadius};
+  font-weight: 300;
+  font-family: Work Sans;
+  font-size: ${theme.font16};
+  outline: none;
+  transition: ${theme.transition};
+
+  &:focus {
+    border: 1px solid ${color.primary};
+    box-shadow: ${theme.boxShadow150};
+  }
+  &::placeholder {
+    color: ${color.text400};
+  }
+`;
+
+const InputTitle = styled.label`
+  position: absolute;
+  top: -20px;
+  font-size: ${theme.font16};
+  font-weight: 300;
+  font-family: Work Sans;
+`;
+
+const InputError = styled.label`
+  position: absolute;
+  bottom: -20px;
+  font-size: ${theme.font14};
+  font-weight: 300;
+  font-family: Work Sans;
+  color: ${color.red};
 `;
