647f1ba19f9ea5364b7937844bb40c3126e8c33f
brkcrdk <brkcrdk@gmail.com>
2021-05-09 00:17:15 +0300

    Update hooks.

--- hooks/index.js
+++ hooks/index.js
@@ -2,5 +2,6 @@ import useSwiper from './useSwiper';
 import useHeaderScroll from './useHeaderScroll';
 import useSidebar from './useSidebar';
 import useWindowWidth from './useWindowWidth';
+import useForm from './useForm';
 
-export { useSwiper, useHeaderScroll, useSidebar, useWindowWidth };
+export { useSwiper, useHeaderScroll, useSidebar, useWindowWidth, useForm };
