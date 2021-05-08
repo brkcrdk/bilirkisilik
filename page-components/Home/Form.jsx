b3bec05b0d54f05394c1f3d1f57d44e95cb718bd
brkcrdk <brkcrdk@gmail.com>
2021-05-08 19:08:27 +0300

    Add form section to homepage.

--- page-components/Home/Form.jsx
+++ page-components/Home/Form.jsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { SectionContainer } from 'components';
+import { SectionContainer, Input } from 'components';
+import { useState } from 'react';
 
 function Form() {
   const notify = () => toast('Mesajınız iletildi!', { type: 'success' });
+  const [name, setName] = useState('');
 
   const handleTest = () => {
     fetch('/api/send', { method: 'POST' });
@@ -15,6 +17,7 @@ function Form() {
     <SectionContainer title="İletişim Formu">
       <button onClick={notify}>Notify!</button>
       <button onClick={handleTest}>Gönder!</button>
+      <Input value={name} onChange={(e) => setName(e.target.value)} />
       <ToastContainer />
     </SectionContainer>
   );
