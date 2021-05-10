9224520fa0428c2d21795a5bfa477413beb449d3
brkcrdk <brkcrdk@gmail.com>
2021-05-10 23:43:57 +0300

    Add error states to useForm hook.

--- hooks/useForm.js
+++ hooks/useForm.js
@@ -1,20 +1,39 @@
 import { useReducer } from 'react';
 const useForm = () => {
   const initialState = {
-    name: '',
-    email: '',
-    phone: '',
-    description: '',
-    message: '',
+    name: {
+      value: '',
+      error: '',
+    },
+    email: {
+      value: '',
+      error: '',
+    },
+    phone: {
+      value: '',
+      error: '',
+    },
+    description: {
+      value: '',
+      error: '',
+    },
+    message: {
+      value: '',
+      error: '',
+    },
   };
   function reducer(state, action) {
     switch (action.type) {
       case 'name':
-        return { ...state, name: action.payload };
+        return { ...state, name: { ...state.name, value: action.payload } };
+      case 'name_error':
+        return { ...state, name: { ...state.name, error: action.payload } };
       case 'email':
         return { ...state, email: action.payload };
       case 'phone':
-        return { ...state, phone: action.payload };
+        return { ...state, phone: { ...state.phone, value: action.payload } };
+      case 'phone_error':
+        return { ...state, phone: { ...state.phone, error: action.payload } };
       case 'description':
         return { ...state, description: action.payload };
       case 'message':
