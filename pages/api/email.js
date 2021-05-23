0e67350b950bf6a03a9e9602b1fd81dbbb004d50
brkcrdk <brkcrdk@gmail.com>
2021-05-23 23:07:09 +0300

    Add mail message template.

--- pages/api/email.js
+++ pages/api/email.js
@@ -11,22 +11,29 @@ export default async function email(request, response) {
   const mailMessage = {
     to: 'info@kriminalincelemeleri.com',
     from: 'brkcrdk@gmail.com',
-    subject: 'Hello from Node.js',
-    textBody: 'This message was sent using the SocketLabs Node.js library!',
-    htmlBody:
-      '<html>This message was sent using the SocketLabs Node.js library!</html>',
+    subject: subject,
+    textBody: `${message}`,
+    htmlBody: `<html>
+      <ul>
+        <li>Ad Soyad: ${name}</li>
+        <li>Telefon Numarası: ${phone}</li>
+        <li>Email: ${email}</li>
+      </ul>
+      <h5>Mesaj:</h5>
+      <p>${message}</p>
+    </html>`,
     messageType: 'basic',
   };
 
-  // return client.send(mailMessage).then(
-  //   (e) => {
-  //     //Handle successful API call
-  //     console.log(e);
-  //     return response.status(200).json({ message: 'Mesajınız iletildi' });
-  //   },
-  //   (err) => {
-  //     //Handle error making API call
-  //     console.log(err);
-  //   }
-  // );
+  return client.send(mailMessage).then(
+    (e) => {
+      //Handle successful API call
+      console.log(e);
+      return response.status(200).json({ message: 'Mesajınız iletildi' });
+    },
+    (err) => {
+      //Handle error making API call
+      console.log(err);
+    }
+  );
 }
