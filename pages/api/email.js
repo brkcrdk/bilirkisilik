09c3ddc9f9fb548b9035ebae24a3641d9fc8b422
brkcrdk <brkcrdk@gmail.com>
2021-05-23 22:47:48 +0300

    Update email api function.

--- pages/api/email.js
+++ pages/api/email.js
@@ -4,21 +4,21 @@ const serverId = 39325;
 const injectionApiKey = 'k2YAg9b8ERo36ZqDj75H';
 
 const client = new SocketLabsClient(serverId, injectionApiKey);
-const message = {
-  to: 'info@kriminalincelemeleri.com',
-  from: 'brkcrdk@gmail.com',
-  subject: 'Hello from Node.js',
-  textBody: 'This message was sent using the SocketLabs Node.js library!',
-  htmlBody:
-    '<html>This message was sent using the SocketLabs Node.js library!</html>',
-  messageType: 'basic',
-};
 
 export default async function email(request, response) {
-  console.log(request);
-  const { message, email } = request.body;
+  const { name, email, subject, phone, message } = request.body;
 
-  // return client.send(message).then(
+  const mailMessage = {
+    to: 'info@kriminalincelemeleri.com',
+    from: 'brkcrdk@gmail.com',
+    subject: 'Hello from Node.js',
+    textBody: 'This message was sent using the SocketLabs Node.js library!',
+    htmlBody:
+      '<html>This message was sent using the SocketLabs Node.js library!</html>',
+    messageType: 'basic',
+  };
+
+  // return client.send(mailMessage).then(
   //   (e) => {
   //     //Handle successful API call
   //     console.log(e);
